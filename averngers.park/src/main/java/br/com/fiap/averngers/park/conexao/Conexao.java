583f65944513360ca9dba01b79ddb79a3f007069
Logon Aluno <logonaluno@local.com>
2019-09-09 10:32:45 -0300

    feature/JSP andrey me ajuda

--- averngers.park/src/main/java/br/com/fiap/averngers/park/conexao/Conexao.java
+++ averngers.park/src/main/java/br/com/fiap/averngers/park/conexao/Conexao.java
@@ -1,17 +1,15 @@
 package br.com.fiap.averngers.park.conexao;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 
 public class Conexao {
 
 	public static Connection conectar() throws Exception {
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName("oracle.jdbc.drive.OracleDriver");
 		} catch (ClassNotFoundException ex) {
-			System.out.println("Error: unable to load driver class!");
-			System.exit(1);
+			ex.printStackTrace();
 		}
 
 		
